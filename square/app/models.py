3609864dfbb286838bf0323f712f8a6d6f18a0d3
dho <dho@quinten-france.com>
2020-10-29 16:43:33 +0100

    init repository

---
+++ square/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Sensor(models.Model):
+    label = models.CharField(max_length=256)
+
+
+class PeopleCounter(models.Model):
+    sensor = models.CharField(max_length=256)
