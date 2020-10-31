a864ba405c88c291cb149595fc1c391bec0f2c47
dho <dho@quinten-france.com>
2020-10-31 22:10:53 +0100

    change model to match more the exercie

--- square/app/models.py
+++ square/app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-import uuid
+from datetime import date
 # Create your models here.
 
 class PeopleCounter(models.Model):
+    id = models.AutoField(primary_key=True)
     room = models.CharField(max_length=256)
-    people_id = models.CharField(max_length=256, primary_key=True, default=uuid.uuid4())
-    status = models.BooleanField(default=0)
+    people_id = models.CharField(max_length=256)
+    status_in = models.IntegerField(default=0)
+    status_out = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
