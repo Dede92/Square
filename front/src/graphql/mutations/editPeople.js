93eff088ea4b9d4096e458a4c67162430fd14986
dho <dho@quinten-france.com>
2020-10-31 17:42:37 +0100

    fix query parameter

--- front/src/graphql/mutations/editPeople.js
+++ front/src/graphql/mutations/editPeople.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export default gql`
 mutation editSeat($peopleId:String!, $status:Boolean!, $room:String!){
-	editSeat(room:$room, status:$s, peopleId:$n){
+	editSeat(room:$room, status:$status, peopleId:$peopleId){
         peopleId
         room
         status
