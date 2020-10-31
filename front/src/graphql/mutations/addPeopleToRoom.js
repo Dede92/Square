1e44042780364c558d4e9ac676025c158aa0e9ca
dho <dho@quinten-france.com>
2020-10-31 17:31:05 +0100

    fix query

--- front/src/graphql/mutations/addPeopleToRoom.js
+++ front/src/graphql/mutations/addPeopleToRoom.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export default gql`
-mutation createSeat($room:String!, $status:String!, $peopleId:String!){
+mutation createSeat($room:String!, $status:Boolean!, $peopleId:String!){
     createSeat(room:$room, peopleId:$peopleId, status:$status){
         room
         peopleId
