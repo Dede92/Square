b27eb04be5b2e1f474991eab2d5f8a6fb681f065
dho <dho@quinten-france.com>
2020-10-31 17:07:36 +0100

    update button component

--- front/src/components/Button/Button.component.js
+++ front/src/components/Button/Button.component.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const ButtonComponent = () => {
-  return <Button variant="contained" color="primary">Show occupancy</Button>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+      flexGrow: 1,
+  },
+}));
+
+const ButtonComponent = (props) => {
+  const {data, handleOnClick} = props
+  const classes = useStyles();
+
+  return (
+  <div>
+    <Button variant="contained" color="primary" onClick={handleOnClick}>Show occupancy</Button>
+    <div>
+      {data}
+    </div>
+  </div>
+  )
 }
 
 export default ButtonComponent;
