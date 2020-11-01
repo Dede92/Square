67a231966b37b1495d6c41dcb9c4de4e8d4d81f6
dho <dho@quinten-france.com>
2020-11-01 10:31:54 +0100

    add atInstant parameter in gq query

--- square/app/queries.py
+++ square/app/queries.py
@@ -1,18 +1,22 @@
 import graphene
 from .models import PeopleCounter
 from .types import PeopleCounterType, PeopleCountType
-from django.db.models import Sum
+from django.db.models import Sum, Q
 
 class Query(graphene.ObjectType):
     all_people_counters = graphene.List(PeopleCounterType)
-    get_people_counter = graphene.Field(PeopleCountType, room=graphene.String(required=True))
+    get_people_counter = graphene.Field(PeopleCountType, room=graphene.String(required=True), atInstant=graphene.String())
 
     def resolve_all_people_counters(self, info, **kwargs):
         return PeopleCounter.objects.all()
 
     def resolve_get_people_counter(self, info, **kwargs):
-        print(kwargs['room'])
-        a = PeopleCounter.objects.filter(room=kwargs['room']).aggregate(num_in=Sum('status_in'), num_out=Sum('status_out'))
+        q = Q()
+        if 'room' in kwargs:
+            q &= Q(room=kwargs['room'])
+        if 'atInstant' in kwargs:
+            q &= Q(created_at__lte=kwargs['atInstant'])
+        a = PeopleCounter.objects.filter(q).aggregate(num_in=Sum('status_in'), num_out=Sum('status_out'))
         return {
             'room': kwargs['room'],
             'status_in' : 0 if a['num_in'] is None else a['num_in'],
